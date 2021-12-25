d9e41f47fd30c2308ba2679a4dc256bc7f647943
AsgerKiehn <65543164+AsgerKiehn@users.noreply.github.com>
2021-12-25 15:33:55 +0100

    Update FormEmail.js

--- src/components/FormEmail.js
+++ src/components/FormEmail.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function FormEmail() {
     return (
         <div>
-            <form action="https://formsubmit.co/asgerkiehn@hotmail.com.com" method="POST">
+            <form action="https://formsubmit.co/asgerkiehn@hotmail.com" method="POST">
                 <input type="hidden" name="_next" value="https://duah.dk"/>
                 <input type="email" name="email" required/>
                 <input type="text" name="name" required/>
